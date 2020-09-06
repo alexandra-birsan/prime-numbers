174c79ea7d271eb46b00f3bac17e73d0a15e7413
Alexandra Birsan <alex@Alexandras-MacBook-Pro.local>
2020-09-06 13:59:28 +0200

    Introduce request timeout + allow multiple requests in parallel

--- src/main/scala/primenumbers/PrimeNumbersApi.scala
+++ src/main/scala/primenumbers/PrimeNumbersApi.scala
@@ -3,23 +3,25 @@ package primenumbers
 import com.twitter.concurrent.AsyncStream
 import com.twitter.finagle.ThriftMux
 import com.twitter.server.TwitterServer
-import primenumbers.thrift.PrimeNumbersService
 import com.twitter.finagle._
 import com.twitter.finagle.http.service.HttpResponseClassifier
 import com.twitter.finagle.thriftmux.service.ThriftMuxResponseClassifier
-import com.twitter.util.Await
+import com.twitter.util.{Await, Duration}
 import io.finch._
 import io.finch.circe._
 import io.circe.generic.auto._
 import io.finch.syntax.get
+import primenumbers.thrift.PrimeNumbersService
 
 object PrimeNumbersApi extends TwitterServer {
 
   private val port = flag[Int]("port", 8081, "Port for the proxy service server")
+  private val thriftClientRequestTimeout = Duration.fromSeconds(10)
 
-  def main: Unit = {
+  def main(): Unit = {
 
     val client = ThriftMux.client
+      .withRequestTimeout(thriftClientRequestTimeout)
       .withResponseClassifier(ThriftMuxResponseClassifier.ThriftExceptionsAsFailures)
       .build[PrimeNumbersService.MethodPerEndpoint](":8082", "proxy-service-client")
 
--- src/main/scala/primenumbers/PrimeNumbersServer.scala
+++ src/main/scala/primenumbers/PrimeNumbersServer.scala
@@ -1,15 +1,20 @@
 package primenumbers
 
+import java.util.concurrent.Executors
+
 import com.twitter.finagle.ThriftMux
 import com.twitter.finagle.thrift.Protocols
 import com.twitter.finagle.thriftmux.service.ThriftMuxResponseClassifier
 import com.twitter.server.TwitterServer
-import com.twitter.util.{Await, Future}
+import com.twitter.util.{Await, Future, FuturePool}
 import primenumbers.thrift.PrimeNumbersService
+
+import scala.concurrent.ExecutionContext
 
 
 object PrimeNumbersServer extends TwitterServer {
 
+  private val serverThreadPool = ExecutionContext.fromExecutorService(Executors.newCachedThreadPool())
   private val port = flag[Int]("port", 8082, "Port for the prime number server")
 
   def main(): Unit = {
@@ -22,6 +27,7 @@ object PrimeNumbersServer extends TwitterServer {
     val finagledService: PrimeNumbersService.FinagledService = new PrimeNumbersService.FinagledService(service, Protocols.binaryFactory())
 
     val server = ThriftMux.server
+      .withExecutionOffloaded(FuturePool.interruptible(serverThreadPool))
       .withResponseClassifier(ThriftMuxResponseClassifier.ThriftExceptionsAsFailures)
       .withLabel("prime-number-server")
       .serve(s":${port()}", finagledService)
